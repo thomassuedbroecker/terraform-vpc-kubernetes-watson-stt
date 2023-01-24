dc0476d3e015f62f309e728b058f0f6e4967eb11
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2023-01-24 13:09:18 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,182 @@
-# terraform-vpc-kubernetes-watson-stt
+# Run Watson NLP for Embed on an IBM Cloud Kubernetes cluster
+
+This example project has two objectives.
+
+* Create an IBM Cloud Kubernetes cluster in a [`Virtual Private Cloud` (VPC) environment](https://www.ibm.com/topics/vpc) with [Terraform](https://www.terraform.io/)
+* Deploy [Watson NLP for embed`](https://www.ibm.com/docs/en/watson-libraries?topic=watson-natural-language-processing-library-embed-home) to the created cluster with [Helm](https://helm.sh/)
+
+The example project reuses code from project [`Use Terraform to create a VPC and a Kubernetes Cluster on IBM Cloud`](https://github.com/thomassuedbroecker/terraform-vpc-kubernetes).
+
+> Visit the related blog post [`Run Watson NLP for Embed on an IBM Cloud Kubernetes cluster in a Virtual Private Cloud environment`](https://suedbroecker.net/2023/01/12/run-watson-nlp-for-embed-on-an-ibm-cloud-kubernetes-cluster-in-a-virtual-private-cloud-environment/).
+
+### Simplified IBM Cloud architecture diagram
+
+Terraform will create and configure on IBM Cloud:
+
+* 1 x VPC
+
+    * 3 x Security Groups
+
+      * 1 x Default
+      * 2 x Related to the Kubernetes Cluster (created by the Kubernetes Service creation)
+    
+    * 1 x Access control list
+    * 1 x Routing table
+    * 1 x Public gateway
+    * 1 x Virtual Private Endpoint Gateway (created by the Kubernetes Service creation)
+    * 1 x Public load balancer (created by the Kubernetes Service creation)
+
+* 1 x Kubernetes Cluster 
+
+    * Including 3 [fully IBM managed master nodes](https://cloud.ibm.com/docs/containers?topic=containers-cs_ov)
+    * Configured 2 Worker nodes (managed by IBM) ([see responsibilities](https://cloud.ibm.com/docs/containers?topic=containers-responsibilities_iks))
+    * Enabled [Block Storage for VPC](http://ibm.biz/addon-state)
+    * Enabled service endpoint for public and private communication
+
+This is a simplified diagram of the created infrastructure with terraform.
+
+![](images/VPC-Kubernetes-simplified-architecture.drawio.png)
+
+### Prerequisites
+
+To use the bash automation you need to have  following tools to be installed on your local computer: 
+
+* [IBM Cloud CLI](https://cloud.ibm.com/docs/cli?topic=cli-getting-started)
+* Plugin VPC infrastructure
+* Plugin Container-service
+* [Terraform](https://www.terraform.io/)
+* [Helm](https://helm.sh/)
+* [kubectl](https://kubernetes.io/de/docs/tasks/tools/install-kubectl/)
+
+## Example setup
+
+The example setup contains two bash automations:
+
+* [One for Terraform](https://github.com/thomassuedbroecker/terraform-vpc-kubernetes-watson-stt/tree/main/code/terraform_setup)
+* [One for Helm](https://github.com/thomassuedbroecker/terraform-vpc-kubernetes-watson-stt/tree/main/code/helm_setup)
+
+### Step 1: Clone the repo
+
+```sh
+git clone https://github.com/thomassuedbroecker/terraform-vpc-kubernetes-watson-stt.git
+cd terraform-vpc-kubernetes-watson-stt
+```
+
+## Create the Kubernetes cluster and VPC
+
+### Step 1: Navigate to the `terraform_setup`
+
+```sh
+cd code/terraform_setup
+```
+
+### Step 2: Create a `.env` file
+
+```sh
+cat .env_template > .env
+```
+
+### Step 3: Add an IBM Cloud access key to your local `.env` file
+
+```sh
+nano .env
+```
+
+Content of the file:
+
+```sh
+export IC_API_KEY=YOUR_IBM_CLOUD_ACCESS_KEY
+export REGION="us-east"
+export GROUP="tsuedbro"
+```
+
+### Step 4: Verify the global variables in the bash script automation 
+
+Inspect the bash automation [`create_vpc_kubernetes_cluster_with_terraform.sh`](https://github.com/thomassuedbroecker/terraform-vpc-kubernetes-watson-nlp/blob/main/code/terraform_setup/create_vpc_kubernetes_cluster_with_terraform.sh) and adjust the values to your need.
+
+```sh
+nano create_vpc_kubernetes_cluster_with_terraform.sh
+```
+
+```sh
+#export TF_LOG=debug
+export TF_VAR_flavor="bx2.4x16"
+export TF_VAR_worker_count="2"
+export TF_VAR_kubernetes_pricing="tiered-pricing"
+export TF_VAR_resource_group=$GROUP
+export TF_VAR_vpc_name="watson-stt-tsuedbro"
+export TF_VAR_region=$REGION
+export TF_VAR_kube_version="1.25.5"
+export TF_VAR_cluster_name="watson-stt-tsuedbro"
+```
+
+### Step 5: Execute the bash automation
+
+>The creation can take up to 1 hour, depending on the region you use.
+
+```sh
+sh create_vpc_kubernetes_cluster_with_terraform.sh
+```
+
+* Example output:
+
+```sh
+...
+Apply complete! Resources: 5 added, 0 changed, 0 destroyed.
+*********************************
+```
+
+## Deploy Watson NLP embed with Helm
+
+### Step 1: Navigate to the `helm_setup`
+
+```sh
+cd code/helm_setup
+```
+
+### Step 2: Create a `.env` file
+
+```sh
+cat .env_template > .env
+```
+
+### Step 3: Add an IBM Cloud access key to your local `.env` file
+
+```sh
+export IC_API_KEY=YOUR_IBM_CLOUD_ACCESS_KEY
+export IBM_ENTITLEMENT_KEY="YOUR_KEY"
+export IBM_ENTITLEMENT_EMAIL="YOUR_EMAIL"
+export CLUSTER_ID="YOUR_CLUSTER"
+export REGION="us-east"
+export GROUP="tsuedbro"
+```
+
+### Step 4: Execute the bash automation
+
+> _Note:_[Watson Speech to Text API documentation](https://cloud.ibm.com/docs/speech-to-text?topic=speech-to-text-about)
+
+```sh
+sh deploy-watson-stt-to-kubernetes.sh
+```
+
+The script does following steps and the links are pointing to the relevant function in the bash automation:
+
+1. [Log on to IBM Cloud with an IBM Cloud API key.](TBD)
+2. [It ensures that is connected to the cluster.](TBD)
+3. [It creates a `Docker Config File` which will be used to create a pull secret.](TBD)
+4. [It installs the Helm chart for Watson STT embed configured for REST API usage.](TBD)
+5. [It verifies that the container is running and invokes a REST API call inside the `runtime-container` of Watson STT emded.](TBD)
+6. [It verifies that the exposed Kubernetes `URL` with a `load balancer service` is working and invokes a the same REST API call as before from the local machine.](TBD)
+
+* Example output:
+
+```sh
+
+TBD
+
+```
+
+The image below shows the running container on the Kubernetes cluster.
+
+![](images/watson-nlp-kubernetes-01.png)
+
---
+++ code/helm_setup/deploy-watson-stt-to-kubernetes.sh
@@ -0,0 +1,303 @@
+#!/bin/bash
+
+# **************** Global variables
+source ./.env
+
+export HELM_RELEASE_NAME=watson-stt-kubernetes
+export HELM_CHART_NAME=watson-stt-kubernetes
+export DEFAULT_NAMESPACE="default"
+
+# **********************************************************************************
+# Functions definition
+# **********************************************************************************
+
+function loginIBMCloud () {
+    
+    echo ""
+    echo "*********************"
+    echo "loginIBMCloud"
+    echo "*********************"
+    echo ""
+
+    ibmcloud login --apikey $IC_API_KEY
+    ibmcloud target -r $REGION
+    ibmcloud target -g $GROUP
+}
+
+function connectToCluster () {
+
+    echo ""
+    echo "*********************"
+    echo "connectToCluster"
+    echo "*********************"
+    echo ""
+
+    ibmcloud ks cluster config -c $CLUSTER_ID
+}
+
+function createDockerCustomConfigFile () {
+
+    echo ""
+    echo "*********************"
+    echo "createDockerCustomConfigFile"
+    echo "*********************"
+    echo ""
+
+    sed "s+IBM_ENTITLEMENT_KEY+$IBM_ENTITLEMENT_KEY+g;s+IBM_ENTITLEMENT_EMAIL+$IBM_ENTITLEMENT_EMAIL+g" "$(pwd)/custom_config.json_template" > "$(pwd)/custom_config.json"
+    IBM_ENTITLEMENT_SECRET=$(base64 -i "$(pwd)/custom_config.json")
+    echo "IBM_ENTITLEMENT_SECRET: $IBM_ENTITLEMENT_SECRET"
+
+    sed "s+IBM_ENTITLEMENT_SECRET+$IBM_ENTITLEMENT_SECRET+g" $(pwd)/charts/$HELM_CHART_NAME/values.yaml_template > $(pwd)/charts/$HELM_CHART_NAME/values.yaml
+    cat $(pwd)/charts/$HELM_CHART_NAME/values.yaml
+}
+
+function installHelmChart () {
+
+    echo ""
+    echo "*********************"
+    echo "installHelmChart"
+    echo "*********************"
+    echo ""
+
+    TEMP_PATH_ROOT=$(pwd)
+    cd $TEMP_PATH_ROOT/charts
+    
+    helm dependency update ./$HELM_CHART_NAME/
+    helm install --dry-run --debug helm-test ./$HELM_CHART_NAME/
+
+    helm lint $HELM_RELEASE_NAME ./$HELM_CHART_NAME/
+    helm install $HELM_RELEASE_NAME ./$HELM_CHART_NAME/
+
+    verifyDeploment
+    verifyPod
+        
+    cd $TEMP_PATH_ROOT
+}
+
+function uninstallHelmChart () {
+
+    echo ""
+    echo "*********************"
+    echo "uninstallHelmChart"
+    echo "*********************"
+    echo ""
+
+    TEMP_PATH_ROOT=$(pwd)
+    cd $TEMP_PATH_ROOT/charts
+
+    helm uninstall $HELM_CHART_NAME
+
+    cd $TEMP_PATH_ROOT
+}
+
+function verifyWatsonSTTContainer () {
+    
+    echo ""
+    echo "*********************"
+    echo "verifyWatsonSTTContainer"
+    echo "*********************"
+    echo ""
+
+    echo "* Download audio"
+    export FIND="ibm-watson-stt-embed"
+    POD=$(kubectl get pods -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $1;}')
+    echo "Pod: $POD"
+    export FIND="runtime"
+    RESULT=$(kubectl exec --stdin --tty $POD --container $FIND -- curl -sLo example.flac https://github.com/watson-developer-cloud/doc-tutorial-downloads/raw/master/speech-to-text/0001.flac)
+    echo ""
+    echo "Result of download the example audio:"
+    echo ""
+    echo "$RESULT"
+    echo ""
+    echo "* Invocation of REST API audio"
+    echo "Result of the Watson STT REST API invocation:"
+    POD=$(kubectl get pods -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $1;}')
+    echo "Pod: $POD"
+    export FIND="runtime"
+    RESULT=$(kubectl exec --stdin --tty $POD --container $FIND --  curl "http://localhost:1080/speech-to-text/api/v1/recognize" --header "Content-Type: audio/flac"  --data-binary @example.flac)
+    echo ""
+    echo "Result of download the example audio:"
+    echo ""
+    echo "http://localhost:1080/speech-to-text/api/v1/recognize"
+    echo ""
+    echo "$RESULT"
+    echo ""
+
+
+    echo "Verify the running pod on your cluster."
+    kubectl get pods -n $DEFAULT_NAMESPACE
+    echo "Verify in the deployment in the Kubernetes dashboard."
+    echo ""
+    open "https://cloud.ibm.com/kubernetes/clusters/$CLUSTER_ID/overview"
+    echo ""
+
+    read ANY_VALUE
+}
+
+function verifyWatsonSTTLoadbalancer () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyWatsonSTTLoadbalancer"
+    echo "this could take up to 10 min"
+    echo "*********************"
+    echo ""
+
+    verifyLoadbalancer
+
+    SERVICE=watson-stt-container-vpc-nlb
+    EXTERNAL_IP=$(kubectl get svc $SERVICE | grep  $SERVICE | awk '{print $4;}')
+    echo "EXTERNAL_IP: $EXTERNAL_IP"
+    echo "Verify invocation of Watson STT API from the local machine:"
+    curl -sLo example.flac https://github.com/watson-developer-cloud/doc-tutorial-downloads/raw/master/speech-to-text/0001.flac
+    curl "http://localhost:1080/speech-to-text/api/v1/recognize" \
+        --header "Content-Type: audio/flac" \
+        --data-binary @example.flac
+}
+
+# ************ functions used internal **************
+
+
+function verifyLoadbalancer () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyLoadbalancer"
+    echo "*********************"
+    echo ""
+
+    export max_retrys=10
+    j=0
+    array=("watson-stt-container-vpc-nlb")
+    export STATUS_SUCCESS=""
+    for i in "${array[@]}"
+        do
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check for $i: ($j) from max retrys ($max_retrys)"
+            j=0
+            export FIND=$i
+            while :
+            do      
+            ((j++))
+            STATUS_CHECK=$(kubectl get svc $FIND -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $4;}')
+            echo "Status: $STATUS_CHECK"
+            if ([ "$STATUS_CHECK" != "$STATUS_SUCCESS" ] && [ "$STATUS_CHECK" != "<pending>" ]); then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is created ($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                    break
+                elif [[ $j -eq $max_retrys ]]; then
+                    echo "$(date +'%F %H:%M:%S') Maybe a problem does exists!"
+                    echo "------------------------------------------------------------------------"
+                    exit 1              
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 60
+            done
+        done
+}
+
+function verifyDeploment () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyDeploment"
+    echo "*********************"
+    echo ""
+
+    export max_retrys=4
+    j=0
+    array=("ibm-watson-stt-embed")
+    export STATUS_SUCCESS="ibm-watson-stt-embed"
+    for i in "${array[@]}"
+        do
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check for ($i)"
+            j=0
+            export FIND=$i
+            while :
+            do      
+            ((j++))
+            echo "($j) from max retrys ($max_retrys)"
+            STATUS_CHECK=$(kubectl get deployment $FIND -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $1;}')
+            echo "Status: $STATUS_CHECK"
+            if [ "$STATUS_CHECK" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is created"
+                    echo "------------------------------------------------------------------------"
+                    break
+                elif [[ $j -eq $max_retrys ]]; then
+                    echo "$(date +'%F %H:%M:%S') Maybe a problem does exists!"
+                    echo "------------------------------------------------------------------------"
+                    exit 1              
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 10
+            done
+        done
+}
+
+function verifyPod () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyPod could take 10 min"
+    echo "*********************"
+    echo ""
+
+    export max_retrys=10
+    j=0
+    array=("ibm-watson-stt-embed")
+    export STATUS_SUCCESS="1/1"
+    for i in "${array[@]}"
+        do
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check for ($i)"
+            j=0
+            export FIND=$i
+            while :
+            do     
+            ((j++))
+            echo "($j) from max retrys ($max_retrys)"
+            STATUS_CHECK=$(kubectl get pods -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $2;}')
+            echo "Status: $STATUS_CHECK"
+            if [ "$STATUS_CHECK" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is created"
+                    echo "------------------------------------------------------------------------"
+                    break
+                elif [[ $j -eq $max_retrys ]]; then
+                    echo "$(date +'%F %H:%M:%S') Maybe a problem does exists!"
+                    echo "------------------------------------------------------------------------"
+                    exit 1              
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 60
+            done
+        done
+}
+
+
+#**********************************************************************************
+# Execution
+# *********************************************************************************
+
+loginIBMCloud
+
+connectToCluster
+
+createDockerCustomConfigFile
+
+installHelmChart
+
+verifyWatsonSTTContainer
+
+verifyWatsonSTTLoadbalancer
+
+uninstallHelmChart
